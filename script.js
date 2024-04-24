f716ed068c3cbdc92031edf5c8ec7545bdbf65b2
Igor Postolny <74405055+postolny@users.noreply.github.com>
2024-04-24 04:41:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
     var text1 = $('#text1').val().toUpperCase();
     var text2 = $('#text2').val().toUpperCase();
     var paganini = $("#paganini")[0];
-    var risata = $("#risata")[0];
-    if ((text1 == "") || (text2 == "")) {
+    var aiaiai = $("#aiaiai")[0];
+    if ((text1 === "") || (text2 === "")) {
       $("#msg").text("Сначала введите слово!").slideDown().delay(2000).slideUp();
     } else {
-      if ((text1 == "ВЕРОНИКА") && (text2 == "НАЧНУ")) {
+      if ((text1 === "ВЕРОНИКА") && (text2 === "НАЧНУ")) {
         $("#voto").css("display", "inline-block");
         paganini.play();
-        risata.pause();
-        $("#text1,#text2").css("background", "#ffe4b5");
+        aiaiai.pause();
+        $("#text1, #text2").css("background", "#ffe4b5");
       } else {
-        risata.play();
+        aiaiai.play();
         paganini.pause();
       }
     }
